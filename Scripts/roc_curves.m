d08f31a99bbf53502f10659bc4e8a8ad98a63747
cmaumet <c.m.j.maumet@warwick.ac.uk>
2014-06-26 16:00:21 +0100

    Count actual number of samples

--- Scripts/roc_curves.m
+++ Scripts/roc_curves.m
@@ -3,11 +3,10 @@ function [auc auc10 dice] = roc_curves(analysisDir)
         analysisDir = pwd;
     end
 
-    baseRealDir = fullfile(analysisDir, 'real_data');
-    baseRealDirFSL = fullfile(analysisDir, 'real_data_FSL');
+    baseRealDir = fullfile(analysisDir);
 
-    zStatGt = spm_select('FPList', fullfile(baseRealDirFSL, 'MFX_with readjusting', 'stats'), '^zstat.*\.nii$');
-    mask = spm_select('FPList', fullfile(baseRealDirFSL, 'MFX_with readjusting', 'stats'), '^mask.*\.nii$');
+    zStatGt = spm_select('FPList', fullfile(baseRealDir, 'MFX_with readjusting', 'stats'), '^zstat.*\.nii$');
+    mask = spm_select('FPList', fullfile(baseRealDir, 'MFX_with readjusting', 'stats'), '^mask.*\.nii$');
         
     zStatGtData = spm_read_vols(spm_vol(zStatGt));
     logPGt = -log10(normcdf(-zStatGtData));
@@ -19,7 +18,7 @@ function [auc auc10 dice] = roc_curves(analysisDir)
     pVal.GLMRFX = spm_read_vols(spm_vol(spm_select('FPList', fullfile(baseRealDir, 'megaRFX'), '^mega_rfx_minus_log10_p')));
     pVal.PermutZ = spm_read_vols(spm_vol(spm_select('FPList', fullfile(baseRealDir, 'permutZ'), '^lP\+\.hdr$')));
     pVal.PermutCon = spm_read_vols(spm_vol(spm_select('FPList', fullfile(baseRealDir, 'permutCon'), '^lP\+\.hdr$')));
-    pVal.GLMFFX = spm_read_vols(spm_vol(spm_select('FPList', fullfile(baseRealDir, 'megaFFX'), '^mega_ffx_ffx_minus_log10_p\.nii$')));
+    pVal.GLMFFX = spm_read_vols(spm_vol(spm_select('FPList', fullfile(baseRealDir, 'megaFFX'), '^megaFFX_ffx_minus_log10_p\.nii$')));
     pVal.Stouffers = spm_read_vols(spm_vol(spm_select('FPList', fullfile(baseRealDir, 'stouffers'), '^stouffers_ffx_minus_log10_p\.nii$')));
     pVal.StouffersMFX = spm_read_vols(spm_vol(spm_select('FPList', fullfile(baseRealDir, 'stouffersMFX'), '^stouffers_rfx_minus_log10_p\.nii$')));
     pVal.WeightedZ = spm_read_vols(spm_vol(spm_select('FPList', fullfile(baseRealDir, 'weightedZ'), '^weightedz_ffx_minus_log10_p\.nii$')));
@@ -42,7 +41,8 @@ function [auc auc10 dice] = roc_curves(analysisDir)
     zGt = -norminv(10.^(-logPGt(inMaskPositions)));
     transfoZGt = round(zGt*factor)/factor;
     
-    uniqueZvalueGt = unique(transfoZGt);
+    [uniqueZvalueGt, ~, c] = unique(transfoZGt);
+    accumarray(c, ones(numel(c),1))
     
     for iMethod = 1:numel(methods)
         iMethod
@@ -54,8 +54,8 @@ function [auc auc10 dice] = roc_curves(analysisDir)
         logPVal3d = getfield(pVal, methods{iMethod});
 %         plot(logPGt(inMaskPositions), logPVal3d(inMaskPositions)-logPGt(inMaskPositions), '.', 'markers',2)
         numBoxPlots = 50;
-        bland_altman_plot(10.^(-logPVal3d(inMaskPositions)), 10.^(-logPGt(inMaskPositions)), '', numBoxPlots, true, true)
-        title(methods{iMethod})
+%         bland_altman_plot(10.^(-logPVal3d(inMaskPositions)), 10.^(-logPGt(inMaskPositions)), '', numBoxPlots, true, true)
+%         title(methods{iMethod})
         
         toPrint = '';
         
@@ -63,12 +63,14 @@ function [auc auc10 dice] = roc_curves(analysisDir)
         
         
         zData = -norminv(10.^(-data));
+        
+        % For each unique z-value in the GT         
         for i = 1:numel(uniqueZvalueGt);
             if mod(i, 5)==0
                 i
             end
             
-            
+           % Find all positions that correspond to this z-value             
            reps = zData(find(transfoZGt == uniqueZvalueGt(i)));
            
            % Exclude proba=1 (can happen in permutation) cause a pb to compute
@@ -80,6 +82,7 @@ function [auc auc10 dice] = roc_curves(analysisDir)
             meanReps = mean(reps);
             stdReps = std(reps);
                       
+            % Select randomly 200 values out of all available            
             randSelection = randi(numel(reps), 1, 200);
             for r = randSelection
                 if isnan(reps(r)) | isinf(reps(r))
@@ -107,8 +110,8 @@ function [auc auc10 dice] = roc_curves(analysisDir)
 
             idx = idx + 1;
         end
-        auc.(methods{iMethod}) = AUC(TPR.(methods{iMethod}), FPR.(methods{iMethod}), 0, 1)
-        auc10.(methods{iMethod}) = AUC(TPR.(methods{iMethod}), FPR.(methods{iMethod}), 0, 0.1)
+%         auc.(methods{iMethod}) = AUC(TPR.(methods{iMethod}), FPR.(methods{iMethod}), 0, 1)
+%         auc10.(methods{iMethod}) = AUC(TPR.(methods{iMethod}), FPR.(methods{iMethod}), 0, 0.1)
 %         figure(52)
 %         hold on;plot(FPR.(methods{iMethod}), TPR.(methods{iMethod}), 'r-', 'linewidth', 2)
     end
