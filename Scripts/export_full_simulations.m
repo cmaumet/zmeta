e98c8494fc3fba84d4039793bbadb991f1662872
cmaumet <c.m.j.maumet@warwick.ac.uk>
2014-09-25 16:17:08 +0100

    Get expected p out of ordering P (not stat values)

--- Scripts/export_full_simulations.m
+++ Scripts/export_full_simulations.m
@@ -1,8 +1,8 @@
 function export_full_simulations(simuDir)
-    simuDirs = find_dirs('^nStudy', simuDir);
+    simuDirs = find_dirs('^nStudy5_subNum-extreme_large', simuDir);
     
     for i = 1:numel(simuDirs)
-        i
+        disp(['Exporting ' simuDirs{i}])
         filename = ['simu_all_' mat2str(i) '.csv'];
         fid = fopen(fullfile(pwd, filename), 'w');
         
@@ -20,54 +20,37 @@ function export_full_simulations(simuDir)
         
         simuDirs{i} = fullfile(simuDir, simuDirs{i});
         fisherFile = spm_select('FPList', fullfile(simuDirs{i}, 'fishers'), '^fishers_ffx_minus_log10_p\.nii$');
+%         statVal.fishers = spm_read_vols(spm_vol(spm_select('FPList', fullfile(simuDirs{i}, 'fishers'), '^fishers_ffx_statistic.nii$')));
         pVal.fishers = spm_read_vols(spm_vol(fisherFile));
-        if any(isinf(pVal.fishers(:)))
-            error('infinite pvals')
-        end
-        
-%         if any(isinf(pVal.fishers(:)))
-%             % Get the probability
-%             copy_nii_image(fisherFile, add_suffix_to_file_name(fisherFile, '_ORIGINAL'));
-%             statFile = spm_select('FPList', fullfile(simuDirs{i}, 'fishers'), '^fishers_ffx_statistic');
-%             
-%             matlabbatch{1}.spm.util.imcalc.input = {statFile};
-%             matlabbatch{1}.spm.util.imcalc.output = ['fishers_ffx_minus_log10_p.nii'];
-%             matlabbatch{1}.spm.util.imcalc.outdir = {spm_file(statFile, 'path')};
-%             nStudies = regexp(simuDirs{i}, 'nStudy(\d+)', 'tokens');
-%             nStudies = nStudies{1}{1}
-%             
-%             matlabbatch{1}.spm.util.imcalc.expression = ['-log10(cdf(''chi2'',i1, 2*' num2str(nStudies) ', ''upper''))'];
-%             matlabbatch{1}.spm.util.imcalc.options.dmtx = 0;
-%             matlabbatch{1}.spm.util.imcalc.options.dtype = 64;
-%             spm_jobman('run', matlabbatch)
-% 
-%             fisherFile = spm_select('FPList', spm_file(statFile, 'path'), '^fishers_ffx_minus_log10_p\.nii$');
-%             pVal.fishers = spm_read_vols(spm_vol(fisherFile));
-%             if any(isinf(pVal.fishers(:)))
-%                 error('infinite pvals')
-%             end
-%         end
         mystr = print_pvalues('', 'fishers', pVal.fishers, info);
         
-        pVal.GLMRFX = spm_read_vols(spm_vol(spm_select('FPList', fullfile(simuDirs{i}, 'megaRFX'), '^mega_rfx_minus_log10_p')));
+        pVal.GLMRFX = spm_read_vols(spm_vol(spm_select('FPList', fullfile(simuDirs{i}, 'megaRFX'), '^mega_rfx_minus_log10_p.nii$')));
+%         statVal.GLMRFX = spm_read_vols(spm_vol(spm_select('FPList', fullfile(simuDirs{i}, 'megaRFX'), '^spmT_0001.nii$')));
+        
         mystr = print_pvalues(mystr, 'GLMRFX', pVal.GLMRFX, info);
         
         pVal.PermutZ = spm_read_vols(spm_vol(spm_select('FPList', fullfile(simuDirs{i}, 'permutZ'), '^lP\+\.hdr$')));
+%         statVal.PermutZ = spm_read_vols(spm_vol(spm_select('FPList', fullfile(simuDirs{i}, 'permutZ'), '^snpmT\+\.hdr$')));
         mystr = print_pvalues(mystr, 'PermutZ', pVal.PermutZ, info);
         
         pVal.PermutCon = spm_read_vols(spm_vol(spm_select('FPList', fullfile(simuDirs{i}, 'permutCon'), '^lP\+\.hdr$')));
+%         statVal.PermutCon = spm_read_vols(spm_vol(spm_select('FPList', fullfile(simuDirs{i}, 'permutCon'), '^snpmT\+\.hdr$')));
         mystr = print_pvalues(mystr, 'PermutCon', pVal.PermutCon, info);
         
         pVal.GLMFFX = spm_read_vols(spm_vol(spm_select('FPList', fullfile(simuDirs{i}, 'megaFFX'), '^mega_ffx_ffx_minus_log10_p\.nii$')));
+%         statVal.GLMFFX = spm_read_vols(spm_vol(spm_select('FPList', fullfile(simuDirs{i}, 'megaFFX'), '^mega_ffx_statistic\.nii$')));
         mystr = print_pvalues(mystr, 'GLMFFX', pVal.GLMFFX, info);        
         
         pVal.Stouffers = spm_read_vols(spm_vol(spm_select('FPList', fullfile(simuDirs{i}, 'stouffers'), '^stouffers_ffx_minus_log10_p\.nii$')));
+%         statVal.Stouffers = spm_read_vols(spm_vol(spm_select('FPList', fullfile(simuDirs{i}, 'stouffers'), '^stouffers_ffx_statistic\.nii$')));
         mystr = print_pvalues(mystr, 'Stouffers', pVal.Stouffers, info);                
         
         pVal.StouffersMFX = spm_read_vols(spm_vol(spm_select('FPList', fullfile(simuDirs{i}, 'stouffersMFX'), '^stouffers_rfx_minus_log10_p\.nii$')));
+%         statVal.StouffersMFX = spm_read_vols(spm_vol(spm_select('FPList', fullfile(simuDirs{i}, 'stouffersMFX'), '^spmT_0001\.nii$')));
         mystr = print_pvalues(mystr, 'StouffersMFX', pVal.StouffersMFX, info);                
         
         pVal.WeightedZ = spm_read_vols(spm_vol(spm_select('FPList', fullfile(simuDirs{i}, 'weightedZ'), '^weightedz_ffx_minus_log10_p\.nii$')));
+%         statVal.WeightedZ = spm_read_vols(spm_vol(spm_select('FPList', fullfile(simuDirs{i}, 'weightedZ'), '^weightedz_ffx_statistic\.nii$')));        
         mystr = print_pvalues(mystr, 'WeightedZ', pVal.WeightedZ, info);                        
         
         fprintf(fid, '%s', mystr);
@@ -85,16 +68,19 @@ function mystr = print_pvalues(mystr, methodName, minuslog10pvalues, info)
     % All of this is needed instead of using just rank produced by 'order'
     % function because we need to deal with duplicate values and take
     % max(rank)
-    [~, ~, pValuesIdx] = unique(pvalues(:));
-    uniquePValuesRank = cumsum(accumarray(pValuesIdx, ones(size(pvalues(:)))));
-    pvalues_rank = uniquePValuesRank(pValuesIdx);
+    % Need to be done on -statValue rather than pvalues to avoid scale effect
+    % that should not be present in expected pvalues
+    orderByValue = pvalues(:); %-statValues(:);
+    [~, ~, statValuesIdx] = unique(orderByValue(:));
+    uniqueStatValuesRank = cumsum(accumarray(statValuesIdx, ones(size(orderByValue(:)))));
+    pvalues_rank = uniqueStatValuesRank(statValuesIdx);
     
     expected_p = pvalues_rank./(info.nSimuOneDir^3);
     
     % Downsampling    
     digits=2;
-    roundedlog10p = round(minuslog10pvalues*10^digits)/(10^digits);
-    [~, uniquePositions] = unique(roundedlog10p);
+    roundedlog10expectedp = round(-log10(expected_p)*10^digits)/(10^digits);
+    [~, uniquePositions] = unique(roundedlog10expectedp);
     
     minuslog10pvalues = minuslog10pvalues(uniquePositions);
     pvalues = pvalues(uniquePositions);
