f1ccd3762fcc66c4bf63f3a4ce6a76cd9553487a
cmaumet <c.m.j.maumet@warwick.ac.uk>
2015-05-06 10:07:25 +0100

    Query report continued

--- Scripts/NIDM-Results paper/pain_meta_analysis/pain_query_report.py
+++ Scripts/NIDM-Results paper/pain_meta_analysis/pain_query_report.py
@@ -1,7 +1,6 @@
 # import glob
 import os
 from rdflib.graph import Graph
-import json
 
 fsl_pain_data_dir = "/Users/cmaumet/Projects/Meta-analysis/Data/" + \
     "FSL_pain_studies/tntmp"
@@ -10,124 +9,172 @@ studies = next(os.walk(fsl_pain_data_dir))[1]
 con_maps = dict()
 sterr_maps = dict()
 
-activated = 0
+study = studies[0]
 
-studies = [studies[0]]
+owl_file = "https://raw.githubusercontent.com/incf-nidash/nidm/master/nidm/\
+nidm-results/terms/releases/nidm-results_100.owl"
 
+gfeat_dir = os.path.join(
+    fsl_pain_data_dir, study, "gFeat", "flm_05mm.gfeat")
+assert os.path.isdir(gfeat_dir)
 
+nidm_dir = os.path.join(gfeat_dir, "nidm")
+assert os.path.isdir(nidm_dir)
 
-for study in studies:
-    gfeat_dir = os.path.join(
-        fsl_pain_data_dir, study, "gFeat", "flm_05mm.gfeat")
-    assert os.path.isdir(gfeat_dir)
+owl_graph = Graph()
+owl_graph.parse(owl_file, format='turtle')
 
-    nidm_dir = os.path.join(gfeat_dir, "nidm")
-    assert os.path.isdir(nidm_dir)
+nidm_doc = os.path.join(nidm_dir, "nidm.ttl")
+print nidm_doc
+nidm_graph = Graph()
+nidm_graph.parse(nidm_doc, format='turtle')
 
-    nidm_doc = os.path.join(nidm_dir, "nidm.ttl")
-    print nidm_doc
+query = """
+prefix prov: <http://www.w3.org/ns/prov#>
+prefix nidm: <http://purl.org/nidash/nidm#>
 
-    nidm_graph = Graph()
-    nidm_graph.parse(nidm_doc, format='turtle')
+prefix ModelParamEstimation: <http://purl.org/nidash/nidm#NIDM_0000056>
+prefix withEstimationMethod: <http://purl.org/nidash/nidm#NIDM_0000134>
+prefix errorVarianceHomogeneous: <http://purl.org/nidash/nidm#NIDM_0000094>
+prefix SearchSpaceMaskMap: <http://purl.org/nidash/nidm#NIDM_0000068>
+prefix contrastName: <http://purl.org/nidash/nidm#NIDM_0000085>
+prefix StatisticMap: <http://purl.org/nidash/nidm#NIDM_0000076>
+prefix searchVolumeInVoxels: <http://purl.org/nidash/nidm#NIDM_0000121>
+prefix HeightThreshold: <http://purl.org/nidash/nidm#NIDM_0000034>
+prefix userSpecifiedThresholdType: <http://purl.org/nidash/\
+nidm#NIDM_0000125>
+prefix ExtentThreshold: <http://purl.org/nidash/nidm#NIDM_0000026>
 
-    query = """
+SELECT DISTINCT ?est_method ?homoscedasticity ?contrast_name
+        ?search_region_nifti  ?search_vol_vox ?extent_thresh
+        ?user_extent_thresh ?height_thresh ?user_height_thresh WHERE {
+    ?mpe a ModelParamEstimation: .
+    ?mpe withEstimationMethod: ?est_method .
+    ?mpe prov:used ?error_model .
+    ?error_model errorVarianceHomogeneous: ?homoscedasticity .
+    ?stat_map prov:wasGeneratedBy/prov:used/prov:wasGeneratedBy ?mpe ;
+              a StatisticMap: ;
+              contrastName: ?contrast_name .
+    ?search_region prov:wasGeneratedBy/prov:used ?stat_map ;
+                   a SearchSpaceMaskMap: ;
+                   prov:atLocation ?search_region_nifti ;
+                   searchVolumeInVoxels: ?search_vol_vox .
+    ?inference prov:used ?stat_map ;
+               prov:used ?extent_thresh ;
+               prov:used ?height_thresh .
+    ?extent_thresh a ExtentThreshold: ;
+                   userSpecifiedThresholdType: ?user_extent_thresh .
+    ?height_thresh a HeightThreshold: ;
+                   userSpecifiedThresholdType: ?user_height_thresh .
+
+}
+
+"""
+sd = nidm_graph.query(query)
+
+Z_STATISTIC = 'Z-Statistic'
+P_VALUE_FWER = 'p-value FWE'
+P_VALUE_FDR = 'p-value FDR'
+P_VALUE_UNCORRECTED = 'p-value uncorrected'
+
+    # ?inference prov:used ?height_thresh .
+    # ?height_thresh a HeightThreshold: ;
+    #                userSpecifiedThresholdType: ?height_thresh_type ;
+    #                prov:value ?height_thresh_value .
+if sd:
+    for row in sd:
+
+        est_method, homoscedasticity, contrast_name, search_region_nifti, \
+            search_vol_vox, extent_thresh, user_extent_thresh, \
+            height_thresh, user_height_thresh = row
+
+        user_extent_thresh = str(user_extent_thresh)
+        user_height_thresh = str(user_height_thresh)
+
+        if str(contrast_name) == "group mean ac" or \
+           str(contrast_name) == "group mean":
+
+            thresh = {
+                Z_STATISTIC: 'prov:value',
+                P_VALUE_FWER: 'pValueFWER:',
+                P_VALUE_FDR: 'pValueFDR:',
+                P_VALUE_UNCORRECTED: 'pValueUncorrected:'}
+
+# FIXME: add pvalueFDR
+            query = """
     prefix prov: <http://www.w3.org/ns/prov#>
     prefix nidm: <http://purl.org/nidash/nidm#>
 
-    prefix ModelParamEstimation: <http://purl.org/nidash/nidm#NIDM_0000056>
-    prefix withEstimationMethod: <http://purl.org/nidash/nidm#NIDM_0000134>
-    prefix errorVarianceHomogeneous: <http://purl.org/nidash/nidm#NIDM_0000094>
-    prefix SearchSpaceMaskMap: <http://purl.org/nidash/nidm#NIDM_0000068>
-    prefix contrastName: <http://purl.org/nidash/nidm#NIDM_0000085>
-    prefix StatisticMap: <http://purl.org/nidash/nidm#NIDM_0000076>
-    prefix searchVolumeInVoxels: <http://purl.org/nidash/nidm#NIDM_0000121>
-    prefix HeightThreshold: <http://purl.org/nidash/nidm#NIDM_0000034>
-    prefix userSpecifiedThresholdType: <http://purl.org/nidash/nidm#NIDM_0000125>
-    prefix ExtentThreshold: <http://purl.org/nidash/nidm#NIDM_0000026>
+    prefix pValueFWER: <http://purl.org/nidash/nidm#NIDM_0000115>
+    prefix pValueUncorrected: <http://purl.org/nidash/nidm#NIDM_0000116>
 
-    SELECT DISTINCT ?est_method ?homoscedasticity ?contrast_name
-            ?search_region_nifti  ?search_vol_vox ?extent_thresh 
-            ?user_extent_thresh ?height_thresh ?user_height_thresh WHERE {
-        ?mpe a ModelParamEstimation: .
-        ?mpe withEstimationMethod: ?est_method .
-        ?mpe prov:used ?error_model .
-        ?error_model errorVarianceHomogeneous: ?homoscedasticity .
-        ?stat_map prov:wasGeneratedBy/prov:used/prov:wasGeneratedBy ?mpe ;
-                  a StatisticMap: ;
-                  contrastName: ?contrast_name .
-        ?search_region prov:wasGeneratedBy/prov:used ?stat_map ;
-                       a SearchSpaceMaskMap: ;
-                       prov:atLocation ?search_region_nifti ;
-                       searchVolumeInVoxels: ?search_vol_vox .
-        ?inference prov:used ?stat_map ;
-                   prov:used ?extent_thresh ;
-                   prov:used ?height_thresh .
-        ?extent_thresh a ExtentThreshold: ;
-                       userSpecifiedThresholdType: ?user_extent_thresh .
-        ?height_thresh a HeightThreshold: ;
-                       userSpecifiedThresholdType: ?user_height_thresh .
-
+    SELECT DISTINCT ?extent_value ?height_value WHERE {
+        <"""+str(extent_thresh)+"> " + \
+                thresh[user_extent_thresh] + """ ?extent_value .
+        <"""+str(height_thresh)+"> " + \
+                thresh[user_height_thresh]+""" ?height_value .
     }
+            """
 
-    """
-    sd = nidm_graph.query(query)
+            thresholds = nidm_graph.query(query)
+            if thresholds:
+                assert len(thresholds) == 1
+                for th_row in thresholds:
+                    extent_thresh, height_thresh = th_row
 
-        # ?inference prov:used ?height_thresh .
-        # ?height_thresh a HeightThreshold: ;
-        #                userSpecifiedThresholdType: ?height_thresh_type ;
-        #                prov:value ?height_thresh_value .
-    if sd:
-        for row in sd:
-
-            est_method, homoscedasticity, contrast_name, search_region_nifti, \
-            search_vol_vox, extent_thresh, user_extent_thresh, height_thresh, \
-            user_height_thresh = row
-            if str(contrast_name) == "group mean ac" or \
-               str(contrast_name) == "group mean":
-
-                thresh = {
-                    'Z-Statistic': 'prov:value',
-                    'p-value FWE': 'pValueFWER:',
-                    'p-value FDR': 'pValueFDR:',
-                    'p-value uncorrected': 'pValueUncorrected:'}
-
-    # FIXME: add pvalueFDR
-                query = """
-        prefix prov: <http://www.w3.org/ns/prov#>
-        prefix nidm: <http://purl.org/nidash/nidm#>
-
-        prefix pValueFWER: <http://purl.org/nidash/nidm#NIDM_0000115>
-        prefix pValueUncorrected: <http://purl.org/nidash/nidm#NIDM_0000116>
-
-        SELECT DISTINCT ?extent_value ?height_value WHERE {
-            <"""+str(extent_thresh)+"> "+thresh[str(user_extent_thresh)]+""" ?extent_value .
-            <"""+str(height_thresh)+"> "+thresh[str(user_height_thresh)]+""" ?height_value .
-        }
-                """
-
-                thresholds = nidm_graph.query(query)
-                if thresholds:
-                    assert len(thresholds) == 1
-                    for th_row in thresholds:
-                        extent_thresh, height_thresh = th_row
-
-                if homoscedasticity:
-                    variance = 'equal'
+            # Convert all info to text
+            thresh = ""
+            multiple_compa = ""
+            if str(user_extent_thresh) in [P_VALUE_FDR, P_VALUE_FWER]:
+                inference_type = "Cluster-wise"
+                multiple_compa = "with correction for multiple \
+comparisons "
+                thresh = "P < %0.2f" % float(extent_thresh)
+                if user_extent_thresh == P_VALUE_FDR:
+                    thresh += " FDR-corrected"
                 else:
-                    variance = 'unequal'                
+                    thresh += " FWER-corrected"
 
-                print "Estimation was performed using %s and assuming %s \
-variances. Inference was performed using " % (est_method, variance)
+                thresh += " (cluster defining threshold "
+                if user_height_thresh == P_VALUE_UNCORRECTED:
+                    thresh += "P < %0.2f)" % float(height_thresh)
+                if user_height_thresh == Z_STATISTIC:
+                    thresh += "Z > %0.2f)" % float(height_thresh)
+            else:
+                inference_type = "Voxel-wise"
+                if user_height_thresh in [P_VALUE_FDR, P_VALUE_FWER]:
+                    multiple_compa = "with correction for multiple \
+comparisons "
+                    thresh = "P < %0.2f" % float(height_thresh)
+                    if user_height_thresh == P_VALUE_FDR:
+                        thresh += " FDR-corrected"
+                    else:
+                        thresh += " FWER-corrected"
+                elif user_height_thresh == P_VALUE_UNCORRECTED:
+                    thresh = "P < %0.2f uncorrected" % float(height_thresh)
+                elif user_height_thresh == Z_STATISTIC:
+                    thresh = "Z > %0.2f uncorrected" % float(height_thresh)
 
-                print "row:"
-                for el in row:
-                    print str(el)
-                print "\n"
+            if homoscedasticity:
+                variance = 'equal'
+            else:
+                variance = 'unequal'
 
-               
-    else:
-        print "not found"
+            print "-------------------"
+            print "%s was performed assuming %s variances. %s inference\
+was performed %susing a threshold %s. The search volume was made of %d voxels\
+(cf. %s)" % (
+                owl_graph.label(est_method), variance, inference_type,
+                multiple_compa, thresh, int(search_vol_vox),
+                search_region_nifti.replace("file://./", ""))
 
-    # print "%s activated? %d" % (study, study_activated)
+            print "-------------------"
+            # print "row:"
+            # for el in row:
+            #     print str(el)
+            # print "\n"
 
-print float(activated)/len(studies)*100
+else:
+    print "not found"
+
+# print "%s activated? %d" % (study, study_activated)
