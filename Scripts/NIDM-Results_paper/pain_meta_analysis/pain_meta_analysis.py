10d6ca7fc0a5e29f4a9354df735f8906b56fe58a
cmaumet <c.m.j.maumet@warwick.ac.uk>
2015-05-08 14:26:30 +0100

    Compute the mask as intersection of all studies

--- Scripts/NIDM-Results_paper/pain_meta_analysis/pain_meta_analysis.py
+++ Scripts/NIDM-Results_paper/pain_meta_analysis/pain_meta_analysis.py
@@ -1,19 +1,23 @@
 import os
 from rdflib.graph import Graph
-from subprocess import call
+from subprocess import check_call
 import shutil
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 data_dir = os.path.join(SCRIPT_DIR, "data")
 
-print data_dir
-
 studies = next(os.walk(data_dir))[1]
-
 print studies
 
 con_maps = dict()
 sterr_maps = dict()
+mask_maps = dict()
+
+ma_mask_name = "meta_analysis_mask"
+ma_mask = None
+
+SPM_SOFTWARE = "http://neurolex.org/wiki/nif-0000-00343"
+FSL_SOFTWARE = "http://neurolex.org/wiki/birnlex_2067"
 
 for study in studies:
     nidm_dir = os.path.join(data_dir, study)
@@ -35,18 +39,22 @@ for study in studies:
     prefix statistic_map: <http://purl.org/nidash/nidm#NIDM_0000076>
     prefix mask_map: <http://purl.org/nidash/nidm#NIDM_0000054>
 
-    SELECT ?contrastName ?con_file ?std_file ?mask_file WHERE {
+    SELECT ?contrastName ?con_file ?std_file ?mask_file ?software WHERE {
      ?con_id a contrast_map: ;
           contrast_name: ?contrastName ;
           prov:atLocation ?con_file .
      ?std_id a stderr_map: ;
           prov:atLocation ?std_file .
-     ?con_est a contrast_estimation: .
+     ?con_est a contrast_estimation: ;
+              prov:wasAssociatedWith ?soft_id.
      ?con_id prov:wasGeneratedBy ?con_est .
      ?std_id prov:wasGeneratedBy ?con_est .
      ?con_est prov:used ?mask_id .
      ?mask_id a mask_map: ;
           prov:atLocation ?mask_file .
+     ?soft_id a ?software .
+
+      FILTER(?software NOT IN (prov:SoftwareAgent, prov:Agent))
     }
 
     """
@@ -54,15 +62,63 @@ for study in studies:
 
     if sd:
         for row in sd:
-            con_name, con_file, std_file, mask_file = row
+            con_name, con_file, std_file, mask_file, software = row
 
             if str(con_name) == "pain: group mean ac" or \
                str(con_name) == "pain: group mean" or \
                str(con_name) == "Group: pain":
-                con_maps[study] = str(con_file).replace("file://.", nidm_dir)
+
+                if str(software) == SPM_SOFTWARE:
+                    # If study was performed with SPM, reslice to FSL's
+                    # template space
+                    for to_reslice in [con_file, std_file, mask_file]:
+                        file_name = os.path.basename(to_reslice).split(".")[0]
+                        check_call(
+                            ["cd " + nidm_dir + ";" +
+                             " flirt -in " + file_name + " -ref " +
+                             "$FSLDIR/data/standard/MNI152_T1_2mm -applyxfm " +
+                             "-usesqform -out " + file_name + "_r"],
+                            shell=True)
+                    for to_rescale in [con_file, std_file]:
+                        file_name = os.path.basename(to_rescale).split(".")[0]
+                        check_call(
+                            ["cd " + nidm_dir + ";" +
+                             " fslmaths " + file_name + "_r -mul 40 " +
+                             file_name + "_rs"],
+                            shell=True)
+                    con_file = con_file.replace(".nii", "_rs.nii")
+                    std_file = std_file.replace(".nii", "_rs.nii")
+                    mask_file = mask_file.replace(".nii", "_r.nii")
+
+                mask_file = mask_file.replace("file://.", nidm_dir)
+
+                if ma_mask is None:
+                    ma_mask = mask_file
+                else:
+                    print [" fslmaths " + mask_file + " -min " +
+                           ma_mask + " " + ma_mask_name]
+                    check_call([" fslmaths " + mask_file + " -min " +
+                           ma_mask + " " + ma_mask_name],
+                               shell=True)
+                    ma_mask = ma_mask_name
+
+
+                # elif str(software == FSL_SOFTWARE):
+                #     # If study was performed with FSL, rescale to a target
+                #     # value of 100
+                #     for to_relice in [con_file, std_file]:
+                #         file_name = os.path.basename(to_relice).split(".")[0]
+                #         check_call(
+                #             ["cd " + nidm_dir + ";" +
+                #              " fslmaths " + file_name + " -div 100 " +
+                #              file_name + "_r"],
+                #             shell=True)
+                con_maps[study] = \
+                    str(con_file).replace("file://.", nidm_dir)
                 sterr_maps[study] = \
-                    str(std_file).replace("file://.", nidm_dir)
-                mask = str(mask_file).replace("file://.", nidm_dir)
+                    str(con_file).replace("file://.", nidm_dir)
+                # mask_maps[study] = \
+                #     str(mask_file).replace("file://.", nidm_dir)
             else:
                 print study
                 print "con_name=--"+str(con_name)+"--"
@@ -75,14 +131,25 @@ for study in studies:
         print study
         print "not found"
 
-call(["fslmerge -t cope.nii.gz "+" ".join(con_maps.values())], shell=True)
-call(["fslmerge -t varcope.nii.gz "+" ".join(sterr_maps.values())], shell=True)
+# Binarize the mask
+check_call(["fslmaths "+ ma_mask +" -thr 0.9 -bin "+ ma_mask], shell=True)
+
+to_merge = {'copes': con_maps, 'varcopes': sterr_maps} #, 'masks': mask_maps}
+for file_type, files in to_merge.items():
+    check_call(
+        ["fslmerge -t "+file_type+".nii.gz "+" ".join(files.values())],
+        shell=True)
+# check_call(
+#     ["fslmerge -t varcope.nii.gz "+" ".join(sterr_maps.values())], shell=True)
+# check_call(
+#     ["fslmerge -t mask.nii.gz "+" ".join(mask_maps.values())], shell=True)
 
 # FIXME: should use a combined mask across all studies
-shutil.copyfile(
-    mask,
-    os.path.join(os.path.dirname(os.path.realpath(__file__)), "mask.nii.gz"))
+# print mask
+# shutil.copyfile(
+#     mask,
+#     os.path.join(os.path.dirname(os.path.realpath(__file__)), "mask.nii.gz"))
 
-call(["flameo --cope=cope --vc=varcope --ld=stats --dm=pain_meta_analysis.mat"
+check_call(["flameo --cope=cope --vc=varcope --ld=stats --dm=pain_meta_analysis.mat"
       " --cs=pain_meta_analysis.grp --tc=pain_meta_analysis.con "
-      "--mask=mask.nii.gz --runmode=flame1"], shell=True)
+      "--mask="+ma_mask_name+" --runmode=flame1"], shell=True)
