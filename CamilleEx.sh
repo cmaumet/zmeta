862e6e17630f31d76a76a0f7906d914b713ebd0c
cmaumet <c.m.j.maumet@warwick.ac.uk>
2016-06-20 16:51:08 +0100

    full array and 4h

--- CamilleEx.sh
+++ CamilleEx.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #$ -S /bin/bash
-#$ -l h_rt=00:30:00
+#$ -l h_rt=04:00:00
 #$ -l h_vmem=4G
-#$ -t 1:3
+#$ -t 1:38
 #$ -o log/betw1_$JOB_NAME.o$JOB_ID.$TASK_ID
 #$ -e log/betw1_$JOB_NAME.e$JOB_ID.$TASK_ID
 #$ -cwd
