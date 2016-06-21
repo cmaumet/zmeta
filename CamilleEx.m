523c89a1c5a1ae5ae51f9d4a01fc4465ae470ca2
cmaumet <c.m.j.maumet@warwick.ac.uk>
2016-06-21 11:59:22 +0100

    Missing analyses: n=25, betw=1, with=5/10/20/80, nominal and diff convec

--- CamilleEx.m
+++ CamilleEx.m
@@ -23,10 +23,10 @@ function simulations(baseDir, redo)
     % Number of subject per study
 %     nSubjects = [25 400 100 25]; %[10, 15, 20, 25, 30, 10, 15, 20, 25, 30, 10, 15, 20, 25, 30];
 %     nStudies = numel(nSubjects);
-    nStudiesArray = [50]% 50];%[5 10 25 50];
+    nStudiesArray = [25]% 50];%[5 10 25 50];
     AVG_NUM_SUB = 20;
     NUM_SUB_DIFF = 15;
-    sigmaSquareArray = AVG_NUM_SUB*[4] %[0.25, 0.5, 1, 2, 4];%How to compute z with var = 0?
+    sigmaSquareArray = AVG_NUM_SUB*[0.25 0.5 1 4] %[0.25, 0.5, 1, 2, 4];%How to compute z with var = 0?
     studyVarianceSchemes = {'identical'} %, 'different'}; don't know yet how to deal with uneq var (for FFX!?)
     
     % Between-studies variance (RFX?)
@@ -36,13 +36,13 @@ function simulations(baseDir, redo)
     subjectPerStudiesScheme = {'identical'}%, 'different'};
     
     % Number of studies with software 2 (fraction)
-    nStudiesWithSoftware2 = [0 1/5 0.5];
+    nStudiesWithSoftware2 = [0] %0 1/5 0.5];
     
     % Correction factor with software 2
-    sigmaFactorWithSoftware2 = [1 2 100];
+    sigmaFactorWithSoftware2 = 1 %[1 2 100];
     
     % Study-specific bias due to units mismatch
-    unitMismatch = [true] %[false, true];
+    unitMismatch = [false, true];
     
     % Type of analysis: one-sample (1), two-sample(2), two-sample
     % unbalanced (3)
