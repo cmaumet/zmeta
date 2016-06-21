5693307fad650c892a29aa5fb0d67526018d56d4
cmaumet <c.m.j.maumet@warwick.ac.uk>
2016-06-21 15:19:08 +0100

    Re-running one analysis only

--- CamilleEx.m
+++ CamilleEx.m
@@ -29,7 +29,7 @@ function simulations(baseDir, redo)
     nStudiesArray = [25]% 50];%[5 10 25 50];
     AVG_NUM_SUB = 20;
     NUM_SUB_DIFF = 15;
-    sigmaSquareArray = AVG_NUM_SUB*[0.25 1 4] %[0.25, 0.5, 1, 2, 4];%How to compute z with var = 0?
+    sigmaSquareArray = AVG_NUM_SUB*[0.25] % 1 4] %[0.25, 0.5, 1, 2, 4];%How to compute z with var = 0?
     studyVarianceSchemes = {'identical'} %, 'different'}; don't know yet how to deal with uneq var (for FFX!?)
     
     % Between-studies variance (RFX?)
