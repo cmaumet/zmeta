fca81c06f6e1c083fb9b0757b4bd593a3d0ae643
cmaumet <c.m.j.maumet@warwick.ac.uk>
2016-06-22 14:16:58 +0100

    Recomputing n=50, between=1

--- CamilleEx.m
+++ CamilleEx.m
@@ -26,14 +26,14 @@ function simulations(baseDir, redo)
     % Number of subject per study
 %     nSubjects = [25 400 100 25]; %[10, 15, 20, 25, 30, 10, 15, 20, 25, 30, 10, 15, 20, 25, 30];
 %     nStudies = numel(nSubjects);
-    nStudiesArray = [25]% 50];%[5 10 25 50];
+    nStudiesArray = [50]% 50];%[5 10 25 50];
     AVG_NUM_SUB = 20;
     NUM_SUB_DIFF = 15;
     sigmaSquareArray = AVG_NUM_SUB*[0.25, 0.5, 1, 2, 4];%How to compute z with var = 0?
     studyVarianceSchemes = {'identical'} %, 'different'}; don't know yet how to deal with uneq var (for FFX!?)
     
     % Between-studies variance (RFX?)
-    sigmaBetweenStudiesArray = [0]%  1];
+    sigmaBetweenStudiesArray = [1]%  1];
     
     % Number of subjects per studies     
     subjectPerStudiesScheme = {'identical'}%, 'different'};
