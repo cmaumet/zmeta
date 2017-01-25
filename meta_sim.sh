45214cd36715b515e56655db8ca9711096b94bf8
cmaumet <c.m.j.maumet@warwick.ac.uk>
2017-01-25 15:58:17 +0000

    Only 5 samples

--- meta_sim.sh
+++ meta_sim.sh
@@ -2,7 +2,7 @@
 #$ -S /bin/bash
 #$ -l h_rt=04:00:00
 #$ -l h_vmem=8G
-#$ -t 1:38
+#$ -t 1:5
 #$ -o log/test3_btw1_wth5_10_20_40_k50_nominal_$JOB_NAME.o$JOB_ID.$TASK_ID
 #$ -e log/test3_btw1_wth5_10_20_40_k50_nominal_$JOB_NAME.e$JOB_ID.$TASK_ID
 #$ -cwd
