5ae5ae2656c7c9b334ffff7c23ed4ec1b5cb0fa5
cmaumet <c.m.j.maumet@warwick.ac.uk>
2017-02-09 14:52:24 +0000

    test 1, k=5, btw=1, all wth

--- meta_sim.m
+++ meta_sim.m
@@ -36,13 +36,13 @@ function meta_sim(base_dir, redo, path_to_spm)
     % ---------------
     
     % Number of studies per meta-analysis
-    settings.ks = 25;%[5 10 25 50];
+    settings.ks = 5;%[5 10 25 50];
 
     % Within-study variance
     settings.wth_sigmas = avg_n*[0.25 0.5 1 2 4];
 
     % Between-studies variance (RFX?)
-    settings.btw_sigmas = [0]% [0 1];
+    settings.btw_sigmas = [1]% [0 1];
 
     % Proportion of studies with software 2 (fraction)
     settings.soft_props = [1/5 0.5];
