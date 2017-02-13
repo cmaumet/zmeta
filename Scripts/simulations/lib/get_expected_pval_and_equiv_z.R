1ef7b9365f2983c783fb533cabfdf6de9bbd7cd5
cmaumet <c.m.j.maumet@warwick.ac.uk>
2017-02-13 17:17:46 +0000

    Plot n100

--- Scripts/simulations/lib/get_expected_pval_and_equiv_z.R
+++ Scripts/simulations/lib/get_expected_pval_and_equiv_z.R
@@ -36,7 +36,7 @@ for (simunum in seq(tot_num_simu, 1, -1)){
 	# It is easier to work with the equiv z stat
 	simudat$equivz <- qnorm(simudat$lnp, lower.tail = FALSE, log.p = TRUE)
 
-	simudat$allgroups <- paste(simudat$Between, simudat$Within, simudat$nStudies, simudat$nSimu, simudat$numSubjectScheme, simudat$varScheme, simudat$soft2, simudat$soft2Factor, as.character(simudat$unitMism))
+	simudat$allgroups <- paste(simudat$Between, simudat$Within, simudat$nStudies, simudat$nSubjects, simudat$nSimu, simudat$numSubjectsSame, simudat$WithinSame, simudat$soft2, simudat$soft2Factor, as.character(simudat$unitMism))
 
 	# newsimudat$expectedp <- newsimudat$rankp/newsimudat$nSimu
 	simudat$expectedz <- qnorm(simudat$expectedP, lower.tail = FALSE)
