9f269f7386b418e1fa470fa5893010e82c34420c
cmaumet <c.m.j.maumet@warwick.ac.uk>
2017-02-22 09:38:18 +0000

    Do not compute FLAME1+2

--- meta_sim.m
+++ meta_sim.m
@@ -354,7 +354,7 @@ function meta_sim(base_dir, redo, path_to_spm, within_id)
                                     megaFFX_dir = fullfile(simu_dir, 'megaFFX');
                                     megaFFXFSL_dir = fullfile(simu_dir, 'megaFFX_FSL');
                                     megaMFX_dir = fullfile(simu_dir, 'megaMFX');
-                                    megaMFX2_dir = fullfile(simu_dir, 'megaMFX2');
+                                    % megaMFX2_dir = fullfile(simu_dir, 'megaMFX2');
                                     permutcon_dir = fullfile(simu_dir, 'permutCon');
                                     permutz_dir = fullfile(simu_dir, 'permutZ');
 
@@ -394,7 +394,7 @@ function meta_sim(base_dir, redo, path_to_spm, within_id)
                                         mkdir(permutcon_dir);                            
                                         mkdir(permutz_dir);   
                                         mkdir(megaMFX_dir);   
-                                        mkdir(megaMFX2_dir);   
+                                        % mkdir(megaMFX2_dir);   
                                         mkdir(megaFFXFSL_dir);
                                     end  
 
@@ -438,8 +438,8 @@ function meta_sim(base_dir, redo, path_to_spm, within_id)
                                     % GLM MFX (FLAME 1)
                                     run_mega_mfx(data_dir, megaMFX_dir, analysis_type, [group1_n group2_n], k, fsl_designs_dir, 1)
 
-                                    % GLM MFX (FLAME 1+2)
-                                    run_mega_mfx(data_dir, megaMFX2_dir, analysis_type, [group1_n group2_n], k, fsl_designs_dir, 2)                                    
+                                    % % GLM MFX (FLAME 1+2)
+                                    % run_mega_mfx(data_dir, megaMFX2_dir, analysis_type, [group1_n group2_n], k, fsl_designs_dir, 2)                                    
 
                                     % GLM FFX (via FSL)
                                     run_mega_ffx(data_dir, megaFFXFSL_dir, analysis_type, [group1_n group2_n], k, fsl_designs_dir)
