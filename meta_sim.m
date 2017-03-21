65020eabccc52be9a829a2eded91ea332ce2acc1
cmaumet <c.m.j.maumet@warwick.ac.uk>
2017-03-21 11:44:11 +0000

    test1, k=20, wth all, n=20, btw=1

--- meta_sim.m
+++ meta_sim.m
@@ -36,7 +36,7 @@ function meta_sim(base_dir, redo, path_to_spm, within_id)
     % ---------------
     
     % Number of studies per meta-analysis
-    settings.ks = 50;%[5 10 25 50];
+    settings.ks = 25;%[5 10 25 50];
 
     % Within-study variance
     settings.wth_sigmas_all = avg_n*[0.25 0.5 1 2 4];
@@ -52,7 +52,7 @@ function meta_sim(base_dir, redo, path_to_spm, within_id)
     settings.soft_factors = [2];
 
     % Study-specific bias due to units mismatch
-    settings.unit_mismatches = {'nominal', 'datascl', 'contscl'};
+    settings.unit_mismatches = {'nominal'};% {'nominal', 'datascl', 'contscl'};
 
     % Type of analysis: one-sample (1), two-sample(2), two-sample
     % unbalanced (3)
