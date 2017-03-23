c4117581c9dd81c12ec9d004ceaac4da839ff57b
cmaumet <c.m.j.maumet@warwick.ac.uk>
2017-03-23 17:52:18 +0000

    Update all plots

---
+++ Scripts/simulations/lib/distribution_plots.py
@@ -0,0 +1,69 @@
+import numpy as np
+import scipy
+import matplotlib.pyplot as plt
+
+p_uppers = dict()
+p_lowers = dict()
+
+# Pre-computed confidence interval for QQplots for more effeciency
+data_sizes = [27000, 38*30*30*30]
+for d in data_sizes:
+    x_log_spaced = np.logspace(np.log10(1), np.log10(d), num=500, endpoint=False)
+    x_log_spaced = np.append(x_log_spaced,d) # Manually add endpoint to avoid rounding errors
+    
+    p_uppers[int(d)] = (
+        [t/d for t in x_log_spaced],
+        [scipy.stats.beta.ppf(0.025, t, d-t+1) for t in x_log_spaced]
+    )
+    p_lowers[int(d)] = (
+        [t/d for t in x_log_spaced],
+        [scipy.stats.beta.ppf(0.975, t, d-t+1) for t in x_log_spaced]
+    )
+
+def distribution_plot(title, data, dist, *args, **kwargs):
+    np.random.seed(0)
+    num_bins = 100
+    
+
+    f, (ax1, ax2) = plt.subplots(1, 2, sharey=False, figsize=(10,5))
+
+    # histogram plot
+    n, bins, patches = ax1.hist(data, num_bins, normed=True)
+
+#     # fit distribution and estimate parameters
+#     param = dist.fit(data)
+#     print(param)
+#     y = dist.pdf(bins, *param[:-2], loc=param[-2], scale=param[-1])
+    
+    # known distribution
+    y = dist.pdf(bins, *args)
+    
+    ax1.plot(bins, y, '-')
+    ax1.set_title(title)
+    
+    # qq-plot plot
+    p_th = [t/data.size for t in range(1,data.size+1)]
+
+    line1, = ax2.loglog(p_th, sorted(dist.sf(data, *args)), '.', linewidth=1,
+                     label=title, markersize=3)
+    ax2.plot(p_th, p_th, '-')
+    x, p_upper = p_uppers[int(data.size)]
+    ax2.plot(x, p_upper, 'c-')
+    x, p_lower = p_lowers[int(data.size)]
+    ax2.plot(x, p_lower, 'c-')   
+    ax2.set_title('Q-Q plot')
+
+
+    ax2.legend(loc='lower right')
+    plt.gca().invert_yaxis()
+    plt.gca().invert_xaxis()
+    plt.show()
+    
+def chi2_distribution_plot(data, title, *args, **kwargs):   
+    distribution_plot(title, data, scipy.stats.chi2, *args)
+    
+def t_distribution_plot(data, title, *args, **kwargs):   
+    distribution_plot(title, data, scipy.stats.t, *args)
+
+def z_distribution_plot(data, title, *args, **kwargs):    
+    distribution_plot(title, data, scipy.stats.norm, *args)
