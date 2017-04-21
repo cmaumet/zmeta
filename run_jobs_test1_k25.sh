201ee0083aa9a5370375db4e0b0fc53509c662f5
cmaumet <c.m.j.maumet@warwick.ac.uk>
2017-04-21 16:36:04 +0100

    comma-sep variables

--- run_jobs_test1_k25.sh
+++ run_jobs_test1_k25.sh
@@ -1,5 +1,5 @@
-qsub -v wth_id=1 k_id=25 test_id=1 meta_sim.sh
-qsub -v wth_id=2 k_id=25 test_id=1 meta_sim.sh
-qsub -v wth_id=3 k_id=25 test_id=1 meta_sim.sh
-qsub -v wth_id=4 k_id=25 test_id=1 meta_sim.sh
-qsub -v wth_id=5 k_id=25 test_id=1 meta_sim.sh
+qsub -v wth_id=1,k_id=25,test_id=1,btw=1:2 meta_sim.sh
+qsub -v wth_id=2,k_id=25,test_id=1,btw=1:2 meta_sim.sh
+qsub -v wth_id=3,k_id=25,test_id=1,btw=1:2 meta_sim.sh
+qsub -v wth_id=4,k_id=25,test_id=1,btw=1:2 meta_sim.sh
+qsub -v wth_id=5,k_id=25,test_id=1,btw=1:2 meta_sim.sh
