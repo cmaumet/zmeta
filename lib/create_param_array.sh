ff5fc4691d8ae61a19cc1179a8b87ffa2bf2cd4f
cmaumet <camille.maumet@inria.fr>
2024-12-20 14:02:24 +0100

    full param array

---
+++ lib/create_param_array.sh
@@ -0,0 +1,16 @@
+for i in {1..38}
+do
+    for j in {1..5}
+    do
+        for k in {1..4}
+        do
+            for l in {1..3}
+            do
+                for m in {1..2}
+                do
+                    echo "$i $j $k $l $m 1"
+                done
+            done
+        done
+    done
+done
