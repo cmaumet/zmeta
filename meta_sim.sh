b5fe25b119d391e2d476136adb35a66a4749cf54
Camille Maumet <camille.maumet@inria.fr>
2024-12-20 15:16:15 +0100

    fix

--- meta_sim.sh
+++ meta_sim.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 
 #OAR -q production 
 #OAR -l host=1/core=1
@@ -10,19 +11,17 @@
 
 echo $OAR_ARRAY_INDEX,$OAR_ARRAY_ID,$1,$2,$3,$4,$5,$6 >> /home/$USER/logs/jobids.csv
 
-source ~/.bashrc
-
 cwd=`pwd`
-cd $HOME
+cd $HOME/code/zmeta_buster
 
-cat > octave_cmd.sh <<EOF
+cat > $PWD/octave_cmd.sh <<EOF
 addpath(pwd);
-addpath(fullfile(pwd, 'lib');
+addpath(fullfile(pwd, 'lib'));
 pkg load statistics
 
-meta_sim('/home/cmaumet/simus',true,fullfile(pwd, '..', 'spm12'), $1, $2, $3, $4, $5, $6);quit"
+meta_sim('/home/cmaumet/simus',true,fullfile(pwd, '..', 'spm12-r7771'), $1, $2, $3, $4, $5, $6)
 EOF
 
-octave octave_cmd.sh
+guix shell octave -- octave $PWD/octave_cmd.sh
 
 cd $cwd
