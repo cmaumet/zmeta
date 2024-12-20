599848b93f34ac7043e456a09a1ff772f33389dc
cmaumet <camille.maumet@inria.fr>
2024-12-20 22:20:36 +0100

    octave cmd file must be different for each job

--- meta_sim.sh
+++ meta_sim.sh
@@ -14,15 +14,22 @@ echo $OAR_ARRAY_INDEX,$OAR_ARRAY_ID,$1,$2,$3,$4,$5,$6 >> /home/$USER/logs/jobids
 cwd=`pwd`
 cd $HOME/code/zmeta_buster
 
-cat > $PWD/octave_cmd.sh <<EOF
+tempfile=$(mktemp)
+
+cat > $tempfile <<EOF
 addpath(pwd);
 addpath(fullfile(pwd, 'lib'));
 pkg load statistics
 
-meta_sim('/srv/storage/empenn@storage2.rennes.grid5000.fr/cmaumet/simus/',false,fullfile(pwd, '..', 'spm12-r7771'), $1, $2, $3, $4, $5, $6)
+meta_sim('/srv/storage/empenn@storage2.rennes.grid5000.fr/cmaumet/simus/',false,fullfile(pwd, '..', 'spm12-r7771'), task_id=$1, within_id=$2, k_id=$3, test_id=$4, btw_id=$5, avgn_id=$6)
 EOF
 
+cat $tempfile
+
 export PATH=/home/cmaumet/fsl/share/fsl/bin:$PATH
-guix shell octave -- octave $PWD/octave_cmd.sh
+guix shell octave -- octave $tempfile
+
+rm $tempfile
 
 cd $cwd
+
