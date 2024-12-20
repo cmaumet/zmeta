d4652f6d11b208d57901e19c23441d58ea172e19
cmaumet <camille.maumet@inria.fr>
2024-12-20 14:45:23 +0100

    limit nb of jobs

--- lib/create_param_array.sh
+++ lib/create_param_array.sh
@@ -1,15 +1,21 @@
-for i in {1..38}
+# bash create_param_array.sh | split -l 200 - batch_
+
+for iter in {1..38}
 do
-    for j in {1..5}
+    for wth_id in {1..5}
     do
-        for k in {1..4}
+        for k_id in {1..4}
         do
-            for l in {1..3}
+            for test_id in {1..3}
             do
-                for m in {1..2}
-                do
-                    echo "$i $j $k $l $m 1"
-                done
+                if [ $k_id -lt 3 ]; then
+                    echo "$iter $wth_id $k_id $test_id 1:2 1"
+                else
+                    for btw_id in {1..2}
+                    do
+                        echo "$iter $wth_id $k_id $test_id $btw_id 1"
+                    done
+                fi
             done
         done
     done
