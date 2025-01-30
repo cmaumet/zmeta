47a24504fd6088f78f562bec2970bd1ac803b5a9
Camille Maumet <camille.maumet@inria.fr>
2025-01-30 13:21:32 +0100

    symlink not working in spm_select

--- run_export.m
+++ run_export.m
@@ -1,3 +1,3 @@
 addpath('~/code/zmeta/Scripts')
 addpath('~/code/spm12-r7771')
-export_full_simulations('~/empenn_group_storage/cmaumet/simus', false)
+export_full_simulations('/srv/storage/empenn@storage2.rennes.grid5000.fr/cmaumet/simus', false)
