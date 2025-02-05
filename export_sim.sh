6572c11fcfd743c02a431cec53ccb8914bf29962
Camille Maumet <camille.maumet@inria.fr>
2025-02-05 12:30:56 +0100

    export sims using cluster

---
+++ export_sim.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -x
+
+#OAR -q production 
+#OAR -l host=1/core=1
+#OAR -l walltime=2:00:00
+#OAR -O OAR_%jobid%.out
+#OAR -E OAR_%jobid%.err 
+#OAR -n run_export
+
+echo "params " $1 
+
+
+tempfile=$(mktemp)
+
+cat > $tempfile <<EOF
+addpath(pwd);
+
+run_export($1)
+EOF
+
+cat $tempfile
+
+export PATH=/home/cmaumet/fsl/share/fsl/bin:$PATH
+guix shell octave -- octave $tempfile
+
+rm $tempfile
+
---
+++ run_export.sh
@@ -0,0 +1,3 @@
+oarsub \
+    -S ./export_sim.sh \
+    --array-param-file export_params
