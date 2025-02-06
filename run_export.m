be2df148c7c6a005075768bdf4452ceda825fcb0
Camille Maumet <camille.maumet@inria.fr>
2025-02-06 11:52:05 +0100

    renamed file

--- run_export.m
+++
@@ -1,4 +0,0 @@
-addpath('~/code/zmeta/Scripts')
-addpath('~/code/spm12-r7771')
-ndatapoints = 30*30*30*38
-export_full_simulations(ndatapoints, '/srv/storage/empenn@storage2.rennes.grid5000.fr/cmaumet/simus', false)
