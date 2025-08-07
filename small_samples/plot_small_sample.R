9b06d26512e1f63953d9b2b6638a061856010e47
cmaumet <camille.maumet@inria.fr>
2025-08-07 10:03:07 +0200

    notebook to R scipts conversion

---
+++ small_samples/plot_small_sample.R
@@ -0,0 +1,124 @@
+
+# Robustness under assumption violations
+
+# # for all methods
+require('cowplot')
+library('ggplot2')
+source(file.path('..','commons','get_expected_pval_and_equiv_z.R'))
+source(file.path('..','commons','multiplot.R'))
+source(file.path('..','commons','plot_lib.R'))
+source(file.path('..', 'commons','prepare_data.R'))
+source(file.path('..', 'commons','plot_unit_mismatch.R'))
+source(file.path('..', 'commons','plot_blandaldman_z.R'))
+source(file.path('..', 'commons','plot_grid_methods_color_within.R'))
+source(file.path('..', 'commons','plot_qq_p.R'))
+# theme_set(theme_gray()) # switch to default ggplot2 theme for good
+# theme_update(panel.background = element_rect(fill = "grey95"))
+# library(scales)
+
+iter = 38
+
+# # One-sample tests
+# # Load data from CSV
+allsimudat_k05 <- load_data_from_csv('^test2_k005_n20.*_nominal', '/Users/camaumet/simus/', iter)
+allsimudat_k10 <- load_data_from_csv('^test2_k010_n20.*_nominal', '/Users/camaumet/simus/', iter)
+allsimudat_k25 <- load_data_from_csv('^test2_k025_n20.*_nominal*', '/Users/camaumet/simus/', iter)
+allsimudat_k50 <- load_data_from_csv('^test2_k050_n20.*_nominal*', '/Users/camaumet/simus/', iter)
+allsimudat_k25_n100 <- load_data_from_csv('^test2_k025_n100.*_nominal', '/Users/camaumet/simus/', iter)
+
+allsimudat <- rbind(
+                    allsimudat_k05, 
+                    allsimudat_k10, 
+                    allsimudat_k25, 
+                    allsimudat_k25_n100,
+                    allsimudat_k50
+)
+
+# # colorblind-friendly palette
+# # Source: http://www.cookbook-r.com/Graphs/Colors_(ggplot2)/
+cbfPalette <- c("#999999", "#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
+# # Figure
+source(file.path('..', 'commons','plot_grid_methods_color_within.R'))
+source(file.path('..', 'commons','plot_qq_p.R'))
+
+# Create a three-panel figure investigating robustness under:
+#  A. small sample sizes (FFX and MFX)
+#  B. heteroscedasticity (RFX and contrast permutation)
+#  C. heterogeneity (Fisher's, Stouffer's, FFX and Weighted Z)
+robutness_plot <- function(data){
+    
+    data_positive_z <- subset(data, expectedz>0 & (unitMism=="nominal"))
+        
+    large_sample_methods <- c("megaMFX","megaFFX_FSL")
+    homoscedasticity_methods <- c("megaRFX","permutCon")
+    homogeneity_methods <- c("megaFFX_FSL","fishers", "stouffers", "weightedZ")
+
+    # Data verifying random/fixed-effect assumption for each method    
+    data_rfx_assumption <- subset(data_positive_z, 
+    ((methods %in% homogeneity_methods & Between==0) | 
+     (!(methods %in% homogeneity_methods) & Between==1)) )
+    
+    # Data violating random/fixed-effect assumption for each method    
+    data_not_rfx_assumption <- subset(data_positive_z, 
+    ((methods %in% homogeneity_methods & Between==1) | 
+     (!(methods %in% homogeneity_methods) & Between==0)) )
+              
+    # Panel A: small samples
+    p1 <- plot_qq_p(
+            subset(data_rfx_assumption, methods %in% large_sample_methods),
+            formula=methods~nStudies+nSubjects, "Small sample sizes") +
+        theme(legend.position="right")  +
+        scale_fill_manual(values=cbfPalette) + 
+        scale_colour_manual(values=cbfPalette)
+    
+    # Panel B: heteroscedasticity
+    p2 <- plot_qq_p(
+            subset(data_rfx_assumption, methods %in% homoscedasticity_methods &
+                                        nSubjects==20 & nStudies==25+25*(data$glm[1] > 1)),
+            formula=.~methods, "Heteroscedasticity", short=TRUE) + 
+        theme(legend.position="none") + 
+        scale_fill_manual(values=cbfPalette) + 
+        scale_colour_manual(values=cbfPalette)
+
+    # Panel C: heterogeneity
+    p3 <- plot_qq_p(
+            subset(data_not_rfx_assumption, methods %in% homogeneity_methods & 
+                                            nStudies==25+25*(data$glm[1] > 1) & nSubjects==20), 
+            formula=.~methods, 
+            title="Heterogeneity", short=TRUE, lim=10) + 
+        theme(legend.position="none") + ylab(NULL) + 
+        scale_fill_manual(values=cbfPalette) + 
+        scale_colour_manual(values=cbfPalette)
+    
+    # Organise the figure: title, panel A at the top, panel B and C in a second row
+    top_row <- plot_grid(p1, labels = 'A', ncol=1)
+   
+    if (data$glm[1] == 1){
+        # For one-sample tests we have many methods to check under heterogeneity        
+        widths = c(0.4, 0.75)
+    } else {
+        # For two-sample tests we only have one method to check under heterogeneity
+        widths = c(0.6, 0.4)
+    }
+    
+    bottom_row <- plot_grid(p2, p3, labels = c('B', 'C'), ncol=2, rel_widths=widths)
+    
+    p <- plot_grid(top_row, bottom_row, labels = ' ', ncol=1, rel_heights=c(1.7, 1))
+    title <- ggdraw() + draw_label(
+        'Robustness of the meta-analytic estimators under assumption violations')
+    p <- plot_grid(title, p, ncol=1, rel_heights=c(0.1, 1)) + 
+        theme(plot.title=element_text(size=12), text=element_text(size=10))
+
+    return(p)
+}
+
+p <- robutness_plot(allsimudat)
+
+# print on screen
+print(p)
+
+# Save to pdf
+pdf(paste("robustness_2.pdf", sep=""))
+print(p)
+dev.off()
