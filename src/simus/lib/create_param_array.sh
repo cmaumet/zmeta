1ad6f79f259bdd09242d91b1a4a7e9a7d00bc347
cmaumet <camille.maumet@inria.fr>
2025-11-25 16:52:48 +0100

    Revert "add param for nominal versus unit mismatch"
    This reverts commit 5b4f3a8368ea3df7e695ffd9099491c3242be060.

--- src/simus/lib/create_param_array.sh
+++ src/simus/lib/create_param_array.sh
@@ -1,4 +1,5 @@
 # bash create_param_array.sh | split -l 200 - batch_
+
 for iter in {1..38}
 do
     for wth_id in {1..5}
@@ -7,20 +8,14 @@ do
         do
             for test_id in {1..3}
             do
-                for unitmis_id in {1..3}
-                do
-                    if [ $k_id -lt 3 ]; then
-                        echo "$iter $wth_id $k_id $test_id 1:2 1 $unitmis_id"
-                    else
-                        for btw_id in {1..2}
-                        do
-                            echo "$iter $wth_id $k_id $test_id $btw_id 1 $unitmis_id"
-                            if [ $k_id -eq 3 ]; then
-                                echo "$iter $wth_id $k_id $test_id $btw_id 2 $unitmis_id"
-                            fi
-                        done
-                    fi
-                done
+                if [ $k_id -lt 3 ]; then
+                    echo "$iter $wth_id $k_id $test_id 1:2 1"
+                else
+                    for btw_id in {1..2}
+                    do
+                        echo "$iter $wth_id $k_id $test_id $btw_id 1"
+                    done
+                fi
             done
         done
     done
